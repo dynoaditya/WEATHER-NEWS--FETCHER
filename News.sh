aac090073c4a5352cf69fcbef9278bf52d14125c
dynoaditya <81763626+dynoaditya@users.noreply.github.com>
2025-11-21 11:38:16 +0530

    Update News.sh

--- News.sh
+++ News.sh
@@ -1,12 +1,5 @@
 
 #!/bin/bash
-read -p "Enter topic: " TOPIC
-URL="https://news.google.com/rss/search?q=$TOPIC&hl=en-IN&gl=IN&ceid=IN:en"
-RAW=$(curl -s "$URL")
-
-echo "Top News on: $TOPIC"
-echo "$RAW" \
-| grep -o "<title>[^<]*</title>" \
-| sed 's/<title>//;s/<\/title>//' \
-| sed '1d' \
-| head -5
+./weather.sh
+echo ""
+./news.sh
